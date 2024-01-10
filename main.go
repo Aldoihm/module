d1fa23c4119061d8bea3f294cf4888c050c8e212
Aldo-Ubuntu-Predator <ingaldo@sotecmovil.com>
2024-01-10 17:20:10 -0600

    Se agregar el archivo main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/Aldoihm/module/slices"
+)
+
+func main() {
+	list := []string{"EDteam", "gophers", "golang", "sotec"}
+
+	slices.Filter(list, func(item string) bool {
+		return strings.HasPrefix(strings.ToLower(item), "h")
+	})
+
+}
