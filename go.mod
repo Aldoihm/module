b18cb2f49931d97ec14cf8b65f68e777eade4cc6
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-01-11 10:00:45 -0600

    Se agrega el paquete quote

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/Aldoihm/module
 
 go 1.21.6
 
-require golang.org/x/exp v0.0.0-20240110193028-0dcbfd608b1e
+require (
+	golang.org/x/exp v0.0.0-20240110193028-0dcbfd608b1e
+	rsc.io/quote v1.5.2
+)
+
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"strings"
 
 	"github.com/Aldoihm/module/slices"
+	"rsc.io/quote"
 )
 
 func main() {
-	list := []string{"EDteam", "gophers", "golang", "sotec"}
+	list := []string{"EDteam", "gophers", "golang", "sotec", quote.Hello()}
 
 	slices.Filter(list, func(item string) bool {
-		return strings.HasPrefix(strings.ToLower(item), "s")
+		return strings.HasPrefix(strings.ToLower(item), "h")
 	})
 
 }
