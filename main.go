0ded3282c7db994572319467a3cb63a8d268b47a
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-01-11 12:58:59 -0600

    Actualización de versiones mayores, importando los nuevos paquetes

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/Aldoihm/module/slices"
 	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
 )
 
 func main() {
@@ -13,5 +15,5 @@ func main() {
 	slices.Filter(list, func(item string) bool {
 		return strings.HasPrefix(strings.ToLower(item), "h")
 	})
-
+	fmt.Println(quoteV3.Concurrency())
 }
